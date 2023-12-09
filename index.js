0ac2c3043f60e578e24bcef4ec01bf15e08c8f5f
Katy DeCorah <katydecorah@gmail.com>
2023-12-09 11:54:44 -0500

    Update dependencies (#10)
    * Switch to jest
    
    * Remove husky
    
    * Update dependencies
    
    * Update test.yml
    
    * Fix paths
    
    * Use a single file

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-"use strict";
-
-const rule = require("unified-lint-rule");
-const visit = require("unist-util-visit");
-const generated = require("unist-util-generated");
-const jsyaml = require("js-yaml");
-const { checkRules, isRequired } = require("./lib/rules.js");
+import { lintRule } from "unified-lint-rule";
+import { visit } from "unist-util-visit";
+import { generated } from "unist-util-generated";
+import { load } from "js-yaml";
 
 function yaml(ast, file, options) {
   visit(ast, "yaml", visitor);
@@ -12,8 +9,8 @@ function yaml(ast, file, options) {
     if (!generated(node)) {
       try {
         // yaml is valid
-        jsyaml.safeLoad(node.value);
-        const frontmatter = jsyaml.safeLoad(node.value);
+        load(node.value);
+        const frontmatter = load(node.value);
         Object.keys(options).forEach((label) => {
           const rules = options[label];
           const value = frontmatter[label];
@@ -27,4 +24,88 @@ function yaml(ast, file, options) {
   }
 }
 
-module.exports = rule("remark-lint:frontmatter-validator", yaml);
+export default lintRule("remark-lint:frontmatter-validator", yaml);
+
+export const checkRules = (file, rules, label, value, required) => {
+  // if not required and it does not have a value, skip it.
+  if (!required && !value) return;
+  if (rules && rules.maxLength) {
+    isMaxLength(file, label, value, rules.maxLength);
+  }
+  if (rules && rules.type) {
+    isType(file, label, value, rules.type);
+  }
+  if (rules && rules.match) {
+    isMatch(file, label, value, rules.match);
+  }
+  if (rules && rules.oneOf) {
+    if (value && typeof value === "string") value = [value];
+    value.forEach((val) => isOneOf(file, label, val, rules.oneOf));
+  }
+};
+
+export const isMatch = (file, label, value, regex) => {
+  regex = new RegExp(regex);
+  if (!value.match(regex)) {
+    file.message(
+      `The value of \`${label}\` "${value}" does not match the pattern: "${regex}"`
+    );
+  }
+};
+
+export const isMaxLength = (file, label, value, maxLength) => {
+  if (value.length > maxLength)
+    file.message(
+      `The value of \`${label}\` has a maximum length of ${maxLength}, the value you entered "${value}" has a length of ${value.length}`
+    );
+};
+
+export const isOneOf = (file, label, value, options) => {
+  if (options.indexOf(value) === -1)
+    file.message(
+      `The value of \`${label}\` "${value}" is not a valid option. Choose from: ${options.join(
+        ", "
+      )}`
+    );
+};
+
+export const isRequired = (file, label, field) => {
+  if (!field) file.message(`The field \`${label}\` is required`);
+};
+
+export const isType = (file, label, value, type) => {
+  switch (type) {
+    case "string":
+      if (!isTypeString(value)) createMessage(file, label, type, value);
+      break;
+    case "object":
+      if (!isTypeObject(value)) createMessage(file, label, type, value);
+      break;
+    case "number":
+      if (!isTypeNumber(value)) createMessage(file, label, type, value);
+      break;
+    case "boolean":
+      if (!isTypeBoolean(value)) createMessage(file, label, type, value);
+      break;
+    case "array":
+      if (!isTypeArray(value)) createMessage(file, label, type, value);
+      break;
+    case "date":
+      if (!isTypeDate(value)) createMessage(file, label, type, value);
+      break;
+    default:
+      file.message(`The type "${type}" is invalid.`);
+  }
+};
+
+const createMessage = (file, label, type, value) =>
+  file.message(
+    `The value of \`${label}\` must be "${type}", it is currently "${typeof value}"`
+  );
+
+const isTypeString = (value) => typeof value === "string";
+const isTypeObject = (value) => typeof value === "object";
+const isTypeNumber = (value) => typeof value === "number";
+const isTypeBoolean = (value) => typeof value === "boolean";
+const isTypeArray = (value) => Array.isArray(value);
+const isTypeDate = (value) => value instanceof Date;
--- test/index.test.js
+++ test/index.test.js
@@ -1,82 +1,301 @@
-"use strict";
+import { remark } from "remark";
+import plugin from "../index.js";
+import frontmatter from "remark-frontmatter";
+import { readSync } from "to-vfile";
 
-const remark = require("remark");
-const plugin = require("../index.js");
-const frontmatter = require("remark-frontmatter");
-const vfile = require("to-vfile");
-const test = require("tape");
+describe("remark-frontmatter-validator", () => {
+  test("the works", () => {
+    remark()
+      .use(frontmatter, ["yaml"])
+      .use(plugin, {
+        tags: {
+          maxLength: 5,
+          type: "object",
+          oneOf: [
+            "CodePen",
+            "Sass",
+            "Haml",
+            "Dribbble",
+            "API",
+            "Jekyll",
+            "Node.js",
+            "CSS",
+            "animation",
+            "writing",
+            "AWS",
+            "Google Sheets",
+            "JavaScript",
+            "Ela Conf",
+            "jQuery",
+            "AngularJS",
+            "recipe",
+            "Zapier",
+            "podcasts",
+            "speaking",
+            "Glitch",
+            "Git",
+            "PHP",
+            "GitHub",
+            "Airtable",
+            "Google Docs",
+          ],
+        },
+        pen: {
+          type: "string",
+          maxLength: "7",
+        },
+        image: {
+          match: "^\\d\\d\\d\\d-\\d\\d-\\d\\d-.*.(png|jpg)",
+          type: "string",
+          required: true,
+        },
+        title: {
+          type: "string",
+          required: true,
+        },
+      })
+      .process(readSync("./test/examples/works.md"), (err, data) => {
+        expect(data.messages.length).toBe(0);
+      });
+  });
 
-test("the works", (t) => {
-  remark()
-    .use(frontmatter, ["yaml"])
-    .use(plugin, {
-      tags: {
-        maxLength: 5,
-        type: "object",
-        oneOf: [
-          "CodePen",
-          "Sass",
-          "Haml",
-          "Dribbble",
-          "API",
-          "Jekyll",
-          "Node.js",
-          "CSS",
-          "animation",
-          "writing",
-          "AWS",
-          "Google Sheets",
-          "JavaScript",
-          "Ela Conf",
-          "jQuery",
-          "AngularJS",
-          "recipe",
-          "Zapier",
-          "podcasts",
-          "speaking",
-          "Glitch",
-          "Git",
-          "PHP",
-          "GitHub",
-          "Airtable",
-          "Google Docs",
-        ],
-      },
-      pen: {
-        type: "string",
-        maxLength: "7",
-      },
-      image: {
-        match: "^\\d\\d\\d\\d-\\d\\d-\\d\\d-.*.(png|jpg)",
-        type: "string",
-        required: true,
-      },
-      title: {
-        type: "string",
-        required: true,
-      },
-    })
-    .process(vfile.readSync("./test/examples/works.md"), (err, data) => {
-      t.equal(data.messages.length, 0);
-      t.end();
-    });
-});
+  test("invalid frontmatter", () => {
+    remark()
+      .use(frontmatter, ["yaml"])
+      .use(plugin, {
+        title: {
+          type: "string",
+          required: true,
+        },
+      })
+      .process(readSync("./test/examples/invalid.md"), (err, data) => {
+        expect(data.messages.length).toBe(1);
+        expect(data.messages[0].message).toMatchInlineSnapshot(`
+        "bad indentation of a mapping entry (1:13)
 
-test("invalid frontmatter", (t) => {
-  remark()
-    .use(frontmatter, ["yaml"])
-    .use(plugin, {
-      title: {
-        type: "string",
-        required: true,
-      },
-    })
-    .process(vfile.readSync("./test/examples/invalid.md"), (err, data) => {
-      t.equal(data.messages.length, 1);
-      t.equal(
-        data.messages[0].message,
-        "incomplete explicit mapping pair; a key node is missed; or followed by a non-tabulated empty line at line 1, column 13:\n    title: hello: world!\n                ^"
-      );
-      t.end();
-    });
+         1 | title: hello: world!
+        -----------------^
+         2 | image: 2020-10-31-black-cat.png
+         3 | tags:"
+      `);
+      });
+  });
+
+  test("isMatch", () => {
+    remark()
+      .use(frontmatter, ["yaml"])
+      .use(plugin, {
+        image: {
+          match: ".*.(jpg|png|gif)$",
+        },
+      })
+      .process(readSync("./test/examples/regex.md"), (err, data) => {
+        expect(data.messages.length).toBe(0);
+      });
+  });
+
+  test("isMatch, not ok", () => {
+    remark()
+      .use(frontmatter, ["yaml"])
+      .use(plugin, {
+        image: {
+          match: ".*.(jpg|png)$",
+        },
+      })
+      .process(readSync("./test/examples/regex.md"), (err, data) => {
+        expect(data.messages.length).toBe(1);
+        expect(data.messages[0].message).toMatchInlineSnapshot(
+          `"The value of \`image\` "cat.gif" does not match the pattern: "/.*.(jpg|png)$/""`
+        );
+      });
+  });
+
+  test("isMaxLength", () => {
+    remark()
+      .use(frontmatter, ["yaml"])
+      .use(plugin, {
+        tags: {
+          maxLength: "1",
+          required: true,
+        },
+        id: {
+          maxLength: "2",
+          required: true,
+        },
+      })
+      .process(readSync("./test/examples/length.md"), (err, data) => {
+        expect(data.messages.length).toBe(2);
+        expect(data.messages).toMatchInlineSnapshot(`
+        [
+          [./test/examples/length.md:1:1: The value of \`tags\` has a maximum length of 1, the value you entered "writing,code,JavaScript" has a length of 3],
+          [./test/examples/length.md:1:1: The value of \`id\` has a maximum length of 2, the value you entered "abcd" has a length of 4],
+        ]
+      `);
+      });
+  });
+
+  test("oneOf", () => {
+    remark()
+      .use(frontmatter, ["yaml"])
+      .use(plugin, {
+        tags: {
+          maxLength: "4",
+          type: "object",
+          required: true,
+          oneOf: ["cool", "beans"],
+        },
+      })
+      .process(readSync("./test/examples/works.md"), (err, data) => {
+        expect(data.messages.length).toBe(1);
+        expect(data.messages).toMatchInlineSnapshot(`
+        [
+          [./test/examples/works.md:1:1: The value of \`tags\` "CSS" is not a valid option. Choose from: cool, beans],
+        ]
+      `);
+      });
+  });
+
+  test("oneOf string", () => {
+    remark()
+      .use(frontmatter, ["yaml"])
+      .use(plugin, {
+        category: {
+          type: "string",
+          required: true,
+          oneOf: ["code"],
+        },
+      })
+      .process(readSync("./test/examples/one-of-string.md"), (err, data) => {
+        expect(data.messages.length).toBe(0);
+      });
+  });
+
+  test("isRequired", () => {
+    remark()
+      .use(frontmatter, ["yaml"])
+      .use(plugin, {
+        title: {
+          type: "string",
+          required: true,
+        },
+        img: {
+          type: "string",
+          required: true,
+        },
+      })
+      .process(readSync("./test/examples/works.md"), (err, data) => {
+        expect(data.messages.length).toBe(2);
+        expect(data.messages).toMatchInlineSnapshot(`
+        [
+          [./test/examples/works.md:1:1: The field \`img\` is required],
+          [./test/examples/works.md:1:1: The value of \`img\` must be "string", it is currently "undefined"],
+        ]
+      `);
+      });
+  });
+
+  test("isType, not ok", () => {
+    remark()
+      .use(frontmatter, ["yaml"])
+      .use(plugin, {
+        tags: {
+          maxLength: "4",
+          type: "string",
+          required: true,
+        },
+      })
+      .process(readSync("./test/examples/works.md"), (err, data) => {
+        expect(data.messages.length).toBe(1);
+        expect(data.messages).toMatchInlineSnapshot(`
+        [
+          [./test/examples/works.md:1:1: The value of \`tags\` must be "string", it is currently "object"],
+        ]
+      `);
+      });
+  });
+
+  test("isType, array", () => {
+    remark()
+      .use(frontmatter, ["yaml"])
+      .use(plugin, {
+        tags: {
+          maxLength: "4",
+          type: "array",
+          required: true,
+        },
+      })
+      .process(readSync("./test/examples/works.md"), (err, data) => {
+        expect(data.messages.length).toBe(0);
+      });
+  });
+
+  test("isType, string", () => {
+    remark()
+      .use(frontmatter, ["yaml"])
+      .use(plugin, {
+        title: {
+          type: "string",
+          required: true,
+        },
+      })
+      .process(readSync("./test/examples/works.md"), (err, data) => {
+        expect(data.messages.length).toBe(0);
+      });
+  });
+
+  test("isType, number", () => {
+    remark()
+      .use(frontmatter, ["yaml"])
+      .use(plugin, {
+        order: {
+          type: "number",
+          required: true,
+        },
+      })
+      .process(readSync("./test/examples/works.md"), (err, data) => {
+        expect(data.messages.length).toBe(0);
+      });
+  });
+
+  test("isType, boolean", () => {
+    remark()
+      .use(frontmatter, ["yaml"])
+      .use(plugin, {
+        offline: {
+          type: "boolean",
+          required: true,
+        },
+      })
+      .process(readSync("./test/examples/works.md"), (err, data) => {
+        expect(data.messages.length).toBe(0);
+      });
+  });
+
+  test("isType, date", () => {
+    remark()
+      .use(frontmatter, ["yaml"])
+      .use(plugin, {
+        date: {
+          required: true,
+          type: "date",
+        },
+      })
+      .process(readSync("./test/examples/works.md"), (err, data) => {
+        expect(data.messages.length).toBe(0);
+      });
+  });
+
+  test("isType, object", () => {
+    remark()
+      .use(frontmatter, ["yaml"])
+      .use(plugin, {
+        authors: {
+          required: true,
+          type: "object",
+        },
+      })
+      .process(readSync("./test/examples/works.md"), (err, data) => {
+        expect(data.messages.length).toBe(0);
+      });
+  });
 });
