39378c88044340900ee068294079589f9748a8b9
Katy DeCorah <katydecorah@gmail.com>
2023-12-09 12:06:41 -0500

    Clean up

--- index.js
+++ index.js
@@ -3,13 +3,10 @@ import { visit } from "unist-util-visit";
 import { generated } from "unist-util-generated";
 import { load } from "js-yaml";
 
-function yaml(ast, file, options) {
-  visit(ast, "yaml", visitor);
-  function visitor(node) {
+function validateFrontmatter(ast, file, options) {
+  visit(ast, "yaml", function visitor(node) {
     if (!generated(node)) {
       try {
-        // yaml is valid
-        load(node.value);
         const frontmatter = load(node.value);
         Object.keys(options).forEach((label) => {
           const rules = options[label];
@@ -18,29 +15,33 @@ function yaml(ast, file, options) {
           checkRules(file, rules, label, value, rules.required);
         });
       } catch (err) {
-        file.message(err, node);
+        file.message(err.message, node);
       }
     }
-  }
+  });
 }
 
-export default lintRule("remark-lint:frontmatter-validator", yaml);
+export default lintRule(
+  "remark-lint:frontmatter-validator",
+  validateFrontmatter
+);
 
 export const checkRules = (file, rules, label, value, required) => {
   // if not required and it does not have a value, skip it.
   if (!required && !value) return;
-  if (rules && rules.maxLength) {
+
+  if (rules.maxLength) {
     isMaxLength(file, label, value, rules.maxLength);
   }
-  if (rules && rules.type) {
+  if (rules.type) {
     isType(file, label, value, rules.type);
   }
-  if (rules && rules.match) {
+  if (rules.match) {
     isMatch(file, label, value, rules.match);
   }
-  if (rules && rules.oneOf) {
-    if (value && typeof value === "string") value = [value];
-    value.forEach((val) => isOneOf(file, label, val, rules.oneOf));
+  if (rules.oneOf) {
+    const values = Array.isArray(value) ? value : [value];
+    values.forEach((val) => isOneOf(file, label, val, rules.oneOf));
   }
 };
 
@@ -61,7 +62,7 @@ export const isMaxLength = (file, label, value, maxLength) => {
 };
 
 export const isOneOf = (file, label, value, options) => {
-  if (options.indexOf(value) === -1)
+  if (!options.includes(value))
     file.message(
       `The value of \`${label}\` "${value}" is not a valid option. Choose from: ${options.join(
         ", "
@@ -73,39 +74,24 @@ export const isRequired = (file, label, field) => {
   if (!field) file.message(`The field \`${label}\` is required`);
 };
 
-export const isType = (file, label, value, type) => {
-  switch (type) {
-    case "string":
-      if (!isTypeString(value)) createMessage(file, label, type, value);
-      break;
-    case "object":
-      if (!isTypeObject(value)) createMessage(file, label, type, value);
-      break;
-    case "number":
-      if (!isTypeNumber(value)) createMessage(file, label, type, value);
-      break;
-    case "boolean":
-      if (!isTypeBoolean(value)) createMessage(file, label, type, value);
-      break;
-    case "array":
-      if (!isTypeArray(value)) createMessage(file, label, type, value);
-      break;
-    case "date":
-      if (!isTypeDate(value)) createMessage(file, label, type, value);
-      break;
-    default:
-      file.message(`The type "${type}" is invalid.`);
-  }
+const typeCheckers = {
+  string: (value) => typeof value === "string",
+  object: (value) => typeof value === "object" && value !== null,
+  number: (value) => typeof value === "number",
+  boolean: (value) => typeof value === "boolean",
+  array: (value) => Array.isArray(value),
+  date: (value) => value instanceof Date,
 };
 
-const createMessage = (file, label, type, value) =>
-  file.message(
-    `The value of \`${label}\` must be "${type}", it is currently "${typeof value}"`
-  );
-
-const isTypeString = (value) => typeof value === "string";
-const isTypeObject = (value) => typeof value === "object";
-const isTypeNumber = (value) => typeof value === "number";
-const isTypeBoolean = (value) => typeof value === "boolean";
-const isTypeArray = (value) => Array.isArray(value);
-const isTypeDate = (value) => value instanceof Date;
+export const isType = (file, label, value, type) => {
+  const checkFunction = typeCheckers[type];
+  if (checkFunction) {
+    if (!checkFunction(value)) {
+      file.message(
+        `The value of \`${label}\` must be "${type}", it is currently "${typeof value}"`
+      );
+    }
+  } else {
+    file.message(`The type "${type}" is invalid.`);
+  }
+};
